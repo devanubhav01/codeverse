133f7191a36d6a93c3587cbc825ccde0ad46a7d9
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-11 13:45:56 +0000

    Aug 11, 2026, 7:15 PM

--- C files/Day06.c
+++ C files/Day06.c
@@ -5,8 +5,12 @@ struct node{
     int data;
     struct node *link;
 };
+
 int main(){
     struct node *head=NULL;
     head=(struct node*)malloc(sizeof(struct node));
-    
+    head->data=45;
+    head->link=NULL;
+    printf("%d",head->data);
+    return 0;
 }

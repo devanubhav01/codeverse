58d1194ce70500a22f4e403c4e1feca0115912e3
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-11 14:24:33 +0000

    Aug 11, 2026, 7:54 PM

--- C files/Day10.c
+++ C files/Day10.c
@@ -1,37 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
-struct node{
+
+struct node
+{
     int data;
     struct node *link;
 };
-void print_data(struct node *head) {
-if(head == NULL)
-printf("Linked List is empty");
-struct node *ptr = NULL;
-ptr = head;
-while(ptr != NULL) {
-printf("%d ", ptr->data);
-ptr = ptr->link;
-}
+void print_data(struct node *head)
+{
+    if (head == NULL)
+        printf("Linked List is empty");
+    struct node *ptr = NULL;
+    ptr = head;
+    while (ptr != NULL)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->link;
+    }
 }
 
-int main(){
-    struct node *head=malloc(sizeof(struct node));
-    head->data=45;
-    head->link=NULL;
+int main()
+{
+    struct node *head = malloc(sizeof(struct node));
+    head->data = 45;
+    head->link = NULL;
 
     struct node *ptr = head;
-ptr = add_at_end(ptr, 98);
-ptr = add_at_end(ptr, 3);
-ptr = add_at_end(ptr, 67);
-ptr = head;
-while(ptr != NULL)
-{
-printf("%d ", ptr->data);
-ptr = ptr->link;
-}
+    ptr = add_at_end(ptr, 98);
+    ptr = add_at_end(ptr, 3);
+    ptr = add_at_end(ptr, 67);
+    ptr = head;
+    while (ptr != NULL)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->link;
+    }
     print_data(head);
-    
+
     return 0;
 }

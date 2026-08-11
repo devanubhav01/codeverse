1cf6ff87323c31da0534869f6bd31d64a101e8ea
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-11 14:07:47 +0000

    Aug 11, 2026, 7:37 PM

--- C files/Day08.c
+++ C files/Day08.c
@@ -5,8 +5,16 @@ struct node{
     int data;
     struct node *link;
 };
-
-
+void print_data(struct node *head) {
+if(head == NULL)
+printf("Linked List is empty");
+struct node *ptr = NULL;
+ptr = head;
+while(ptr != NULL) {
+printf("%d ", ptr->data);
+ptr = ptr->link;
+}
+}
 
 int main(){
     struct node *head=malloc(sizeof(struct node));

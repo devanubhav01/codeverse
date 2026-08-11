c70c29a59340ea90be2873ae864d7e7de2c498fa
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-11 14:17:29 +0000

    Aug 11, 2026, 7:47 PM

--- C files/Day09.c
+++ C files/Day09.c
@@ -16,6 +16,19 @@ ptr = ptr->link;
 }
 }
 
+void add_at_end(struct node *head, int data) {
+struct node *ptr, *temp;
+ptr = head;
+temp = (struct node*)malloc(sizeof(struct node));
+temp->data = data;
+temp->link = NULL;
+while(ptr->link != NULL) {
+ptr = ptr->link;
+}
+ptr->link = temp;
+}
+
+
 int main(){
     struct node *head=malloc(sizeof(struct node));
     head->data=45;
@@ -31,12 +44,12 @@ int main(){
     current->link=NULL;
 
     head->link->link=current;
-    print_data(head);
     add_at_end(head, 67);
+    print_data(head);
 
 
 
 
-    
+
     return 0;
 }

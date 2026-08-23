6e26e8dc05866d192ea8e9dba25a427d366d3b3b
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-23 09:34:39 +0000

    Aug 23, 2026, 3:04 PM

--- Java files/Day03.java
+++ Java files/Day03.java
@@ -1,13 +1,13 @@
 class MyEmployee {
     int id;
-    String Name;
+    String name;
 }
 
 public class Day03 {
 public static void main(String[] args) {
     MyEmployee a=new MyEmployee();
     a.id=101;
-    
+    a.name="tesla";
 }
     
 }

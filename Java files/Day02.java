348c6281e296aa622efc098c1e5a7a66be1b7c05
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-23 06:13:40 +0000

    Aug 23, 2026, 11:43 AM

--- Java files/Day02.java
+++ Java files/Day02.java
@@ -17,7 +17,7 @@ public static void  main(String[] args) {
         a.id = 51;
         a.name = "trump";
         b.id = 100;
-        a.name = "kim";
+        b.name = "kim";
         // System.out.println(a.id);
         // System.out.println(a.name);
         a.printDetails();

164f0ee53c1e030dc38cf4f47716319c951913d6
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-23 06:11:43 +0000

    Aug 23, 2026, 11:41 AM

--- Java files/Day02.java
+++ Java files/Day02.java
@@ -3,7 +3,7 @@ class Employee {
     String name;
 
     public void printDetails() {
-        System.out.println("My id is" + id);
+        System.out.println("My id is " + id);
         System.out.println("and My name is "+ name);
     }
 }
@@ -13,6 +13,7 @@ public class Day02 {
 public static void  main(String[] args) {
         System.out.println("This is our custoom class");
         Employee a = new Employee();
+        Employee b = new Employee();
         a.id = 51;
         a.name = "trump";
         // System.out.println(a.id);

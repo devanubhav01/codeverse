da1e786079329c7e016a7f4ea817b9696048d940
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-23 10:11:59 +0000

    Aug 23, 2026, 3:41 PM

--- Java files/Day04.java
+++ Java files/Day04.java
@@ -1,4 +1,24 @@
+class MyEmployee {
+    private int id;
+    private String name;
 
+    public void setName(String z) {
+        name = z;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int v) {
+
+        id = v;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
 
 
 

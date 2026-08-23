488dc656eb5281508fc277ba9396de2e138c5580
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-23 10:22:52 +0000

    Aug 23, 2026, 3:52 PM

--- Java files/Day04.java
+++ Java files/Day04.java
@@ -32,6 +32,6 @@ public class Day04 {
         g.setName("groot");
         g.setId(12);
         System.out.println(g.getName());
-        System.out.println();
+        System.out.println(g.getId());
     }
 }

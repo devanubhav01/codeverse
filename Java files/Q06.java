b9d8fe998667278ff2fddb40214ecfb917982fa9
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-23 08:48:47 +0000

    Aug 23, 2026, 2:18 PM

--- Java files/Q06.java
+++ Java files/Q06.java
@@ -2,15 +2,15 @@ class Circle {
     float radius;
 
     public float area() {
-        return 3.14f*radius*radius;
+        return 3.14f * radius * radius;
     }
+
     public float perimeter() {
-        return 2*3.14f * radius;
+        return 2 * 3.14f * radius;
     }
 }
 
-
- public class Q06 {
+public class Q06 {
     public static void main(String[] args) {
         Circle c = new Circle();
         c.radius = 5;

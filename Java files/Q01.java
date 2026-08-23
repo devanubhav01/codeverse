e924447fe3cd0a4499005a61de59492c36753328
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-23 06:45:07 +0000

    Aug 23, 2026, 12:15 PM

--- Java files/Q01.java
+++ Java files/Q01.java
@@ -1,5 +1,19 @@
 class Employee{
-    
+    int salary;
+
+    public int getSalary() {
+        return salary;
+    }
+
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String z) {
+        name = z;
+    }
 }
 
 public class Q01 {

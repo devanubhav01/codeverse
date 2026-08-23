967a37e748fd81b7aed019687b475ae556ecae31
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-23 09:53:52 +0000

    Aug 23, 2026, 3:23 PM

--- Java files/Day03.java
+++ Java files/Day03.java
@@ -1,13 +1,31 @@
 class MyEmployee {
-    int id;
-    String name;
+   private int id;
+   private String name;
+
+   public void setName(String z) {
+       name = z;
+   }
+
+   public String getName() {
+       return name;
+   }
+
+   public void setId(int v) {
+   
+    id =v;
+ }
+
+   
+  public int getId(){
+      return id;
+   }
 }
 
 public class Day03 {
 public static void main(String[] args) {
     MyEmployee a=new MyEmployee();
-    a.id=101;
-    a.name="tesla";
+    // a.id=101;
+    // a.name="tesla";
 }
     
 }

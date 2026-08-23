a24dadf749e543d85c658ddb0cc5827afac67ac6
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-23 06:05:27 +0000

    Aug 23, 2026, 11:35 AM

--- Java files/Day02.java
+++ Java files/Day02.java
@@ -8,6 +8,6 @@ public class Day02 {
 public static void  main(String[] args) {
         System.out.println("This is our custoom class");
         Employee a = new Employee();
-        
+        a.id=
      }
 }

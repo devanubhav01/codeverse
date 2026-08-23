a7f63a83e9d760df8d379f86dc4f8c8e527f58a6
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-23 06:55:15 +0000

    Aug 23, 2026, 12:25 PM

--- Java files/Q02.java
+++ Java files/Q02.java
@@ -2,8 +2,13 @@ class CellPhone {
     public void ring() {
         System.out.println("Ringing....");
     }
+
     public void vibrate() {
         System.out.println("Vibrating....");
+    }
+
+    public void callFriend() {
+        System.out.println("");
     }
 
 }

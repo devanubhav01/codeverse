268901683b2de21a4455a12101ba5889ff17f54e
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-24 11:07:17 +0000

    Aug 24, 2026, 4:37 PM

--- C files/P16.c
+++ C files/P16.c
@@ -103,6 +103,8 @@ struct node *del_last(struct node *head)
     return head;
 }
 
+//struct node* del_pos(struct node *head,)
+
 int main()
 {
     struct node *head = NULL;
@@ -112,7 +114,7 @@ int main()
     head = add_at_pos(head, 2, 100);
     //head = del_beg(head);
     //head = del_last(head);
-    head = del_pos(head);
+   // head = del_pos(head);
 
     print(head);
 

63b304165fa25bfe1159c37e43c00284fc3dd437
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-24 11:15:50 +0000

    Aug 24, 2026, 4:45 PM

--- C files/P16.c
+++ C files/P16.c
@@ -106,7 +106,9 @@ struct node *del_last(struct node *head)
 struct node* del_pos(struct node *head,int pos){
     struct node *temp = head;
     struct node *temp2 = NULL;
-    
+    while(pos>1){
+        
+    }
 }
 
 int main()

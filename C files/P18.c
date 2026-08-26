bd3502fe1b39c97a26255bc82ad36ce252a96c9d
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-26 03:45:24 +0000

    Aug 26, 2026, 9:15 AM

--- C files/P18.c
+++ C files/P18.c
@@ -47,7 +47,7 @@ int peek(struct stack *z,int i){
     
     printf(" %d \n", pop(z));
 
-    for (int j = 0; j < z->top + 1;j++){
+    for (int j = 1; j < z->top + 1;j++){
         printf("The value at index %d is %d\n", j, peek(z, j));
     }
 

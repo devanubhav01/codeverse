69b9808e7c2f1a8ba5fda83c89cea22fd408e0d0
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-08-26 04:57:26 +0000

    Aug 26, 2026, 10:27 AM

--- C files/P19.C
+++ C files/P19.C
@@ -6,7 +6,20 @@ struct Node{
     struct Node *link;
 };
 
-
+void print_data(struct node *head)
+{
+    if (head == NULL)
+    {
+        printf("empty");
+    }
+    struct Node *ptr = NULL;
+    ptr = head;
+    while (ptr != NULL)
+    {
+        printf(" %d ", ptr->data);
+        ptr = ptr->link;
+    }
+}
 
 int isEmpty(struct Node *top){
     if(top==NULL){
@@ -26,22 +39,35 @@ int isFull(struct node *top){
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+struct Node *push(struct Node *top,int x){
+    if(isFull(top)){
+        printf("StackOverflow\n");
+    }else{
+        struct Node *n = (struct Node *)malloc(sizeof(struct Node));
+        n->data = x;
+        n->link = top;
+        top = n;
+        return top;
+    }
+}
 
 int main(){
-
+    struct Node *top = NULL;
+    top = push(top, 90);
+    print_data(top);
     return 0;
-} 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+

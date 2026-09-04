71f3843f7d32d30c4491b1d28670240ae5ecd143
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-04 04:14:21 +0000

    Sep 4, 2026, 9:44 AM

--- JavaScript files/script10.js
+++ JavaScript files/script10.js
@@ -1,8 +1,10 @@
 let arr = [1, 2, 5, 8, 9, 12]
-let newArr = []
-for (let i = 0; i < arr.length; i++){
-    const element = arr[i]
-    newArr.push(element**2)
-}
+// let newArr = []
+// for (let i = 0; i < arr.length; i++){
+//     const element = arr[i]
+//     newArr.push(element**2)
+// }
+
+
 
 console.log(newArr) 

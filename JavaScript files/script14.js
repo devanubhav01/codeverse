1fd3f5005ec049e49f67469aea1a789d32e758a6
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-04 10:34:05 +0000

    Sep 4, 2026, 4:04 PM

--- JavaScript files/script14.js
+++ JavaScript files/script14.js
@@ -15,17 +15,17 @@
     // Simulate getting data from a server
     // let x = await fetch('https://jsonplaceholder.typicode.com/todos/1')
 
-//     let x = await fetch('https://jsonplaceholder.typicode.com/posts', {
-//                 method: 'POST',
-//                 body: JSON.stringify({
-//                     title: 'foo',
-//                     body: 'bar',
-//                     userId: 1,
-//                 }),
-//                 headers: {
-//                     'Content-type': 'application/json; charset=UTF-8',
-//                 },
-//             })
+    let x = await fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: 'POST',
+                body: JSON.stringify({
+                    title: 'foo',
+                    body: 'bar',
+                    userId: 1,
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            })
      let data = await x.json() 
      return data
  }

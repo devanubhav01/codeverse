81b8adde7bf37492fa3b6fc6f1b3cafaef898f57
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-04 04:09:46 +0000

    Sep 4, 2026, 9:39 AM

--- JavaScript files/script10.js
+++ JavaScript files/script10.js
@@ -1,6 +1,6 @@
 let arr = [1, 2, 5, 8, 9, 12]
 let newArr = []
-for (let i = 0; i < a.length; i++){
+for (let i = 0; i < arr.length; i++){
     const element = arr[i]
     newArr.push(element**2)
 }

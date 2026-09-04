657590c5c74157d43ca8005db9aca7c195aff7c7
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-04 04:04:17 +0000

    Sep 4, 2026, 9:34 AM

--- JavaScript files/script9.js
+++ JavaScript files/script9.js
@@ -9,14 +9,15 @@ let a = [2, 4, 5, 6, 7, 8]
 //     console.log(value,index,arr)
 // })
 
-let obj = {
-    a: 1,
-    b: 2,
-    c:3
-}
-for (const key in obj) {
-    if (Object.hasOwnProperty.call(obj, key)) {
-        const element = obj[key];
-        console.log(key,element)
-    }
-}
+// let obj = {
+//     a: 1,
+//     b: 2,
+//     c:3
+// }
+// for (const key in obj) {
+//     if (Object.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key];
+//         console.log(key,element)
+//     }
+// }
+

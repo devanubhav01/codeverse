ff7bce659e87ee563dc5287c3c4a2dac3a820f05
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-04 04:06:36 +0000

    Sep 4, 2026, 9:36 AM

--- JavaScript files/script9.js
+++ JavaScript files/script9.js
@@ -1,25 +1,25 @@
 let a = [2, 4, 5, 6, 7, 8]
 
-// for (let i = 0; i < a.length; i++){
-//     const element = a[i];
-//     console.log(element)
-// }
+for (let i = 0; i < a.length; i++){
+    const element = a[i];
+    console.log(element)
+}
 
-// a.forEach((value, index, arr) =>{
-//     console.log(value,index,arr)
-// })
+a.forEach((value, index, arr) =>{
+    console.log(value,index,arr)
+})
 
-// let obj = {
-//     a: 1,
-//     b: 2,
-//     c:3
-// }
-// for (const key in obj) {
-//     if (Object.hasOwnProperty.call(obj, key)) {
-//         const element = obj[key];
-//         console.log(key,element)
-//     }
-// }
+let obj = {
+    a: 1,
+    b: 2,
+    c:3
+}
+for (const key in obj) {
+    if (Object.hasOwnProperty.call(obj, key)) {
+        const element = obj[key];
+        console.log(key,element)
+    }
+}
 
 for (const iterator of a) {
    console.log(iterator) 

d43600388dbf97d2ce513b37db67028840b7de48
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-04 18:53:56 +0000

    Sep 5, 2026, 12:23 AM

--- JavaScript files/script15.js
+++ JavaScript files/script15.js
@@ -9,14 +9,14 @@ let sum = parseInt(a) + parseInt(b)
 //console.log("The sum is ",sum)
 // function main() {
 //     let x = 1;
-//     try {
-//         console.log("The sum is ", sum * x)
-//         return true
+    try {
+        console.log("The sum is ", sum * x)
+        return true
 
-//     } catch (error) {
-//         console.log("Error aa gaya bhai")
+    } catch (error) {
+        console.log("Error aa gaya bhai")
 //         return false
-//     }
+     }
 //     finally {
 //         console.log("files are being closed and db connection is being closed")
 //     }

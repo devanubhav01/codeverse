eba7b48db62064585cce15926294c105e8c9c8c2
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-04 18:46:12 +0000

    Sep 5, 2026, 12:16 AM

--- JavaScript files/script15.js
+++ JavaScript files/script15.js
@@ -7,20 +7,20 @@ if (isNaN(a) || isNaN(b)) {
 
 let sum = parseInt(a) + parseInt(b)
 
-function main() {
-    let x = 1;
-    try {
-        console.log("The sum is ", sum * x)
-        return true
+// function main() {
+//     let x = 1;
+//     try {
+//         console.log("The sum is ", sum * x)
+//         return true
 
-    } catch (error) {
-        console.log("Error aa gaya bhai")
-        return false
-    }
-    finally {
-        console.log("files are being closed and db connection is being closed")
-    }
+//     } catch (error) {
+//         console.log("Error aa gaya bhai")
+//         return false
+//     }
+//     finally {
+//         console.log("files are being closed and db connection is being closed")
+//     }
 
-}
+// }
 
-let c = main()
+// let c = main()

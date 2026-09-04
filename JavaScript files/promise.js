165d3c8642c25b629391a64fcac51303d857b638
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-04 09:32:00 +0000

    Sep 4, 2026, 3:02 PM

--- JavaScript files/promise.js
+++ JavaScript files/promise.js
@@ -13,23 +13,26 @@ let prom1 = new Promise((resolve, reject) => {
     }
 })
 
-let prom2 = new Promise((resolve, reject) => {
-    let a = Math.random();
-    if (a < 0.5) {
-        reject("No random number was supporting you 2")
-    }
-    else {
-        setTimeout(() => {
-            console.log("Yes I am done 2")
-            resolve("Groot 2")
-        }, 1000);
-    }
-})
 
 
-let p3 = Promise.race([prom1, prom2])
-p3.then((a) => {
-    console.log(a)
-}).catch(err => {
-    console.log(err)
-})
+
+// let prom2 = new Promise((resolve, reject) => {
+//     let a = Math.random();
+//     if (a < 0.5) {
+//         reject("No random number was supporting you 2")
+//     }
+//     else {
+//         setTimeout(() => {
+//             console.log("Yes I am done 2")
+//             resolve("Groot 2")
+//         }, 1000);
+//     }
+// })
+
+
+// let p3 = Promise.race([prom1, prom2])
+// p3.then((a) => {
+//     console.log(a)
+// }).catch(err => {
+//     console.log(err)
+// })

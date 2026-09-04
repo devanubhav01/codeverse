1a387268fcba2d781cc7ba4eb2108a772e9bdf4a
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-04 05:22:01 +0000

    Sep 4, 2026, 10:52 AM

--- JavaScript files/script12.js
+++ JavaScript files/script12.js
@@ -7,9 +7,9 @@ button.addEventListener("dblclick", () => {
     document.querySelector(".box").innerHTML = "<b>Yayy you were clicked</b> Enjoy your click!"
 })
 
-// button.addEventListener("contextmenu", () => {
-//     alert("Dont hack us by Right click Please")
-// })
+button.addEventListener("contextmenu", () => {
+    alert("Dont hack us by Right click Please")
+})
 
 // button.addEventListener("click", () => {
 //     alert("I was clicked")

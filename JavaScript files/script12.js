959b827dbaacee272737402bbacdb596e5ef2e07
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-04 05:10:12 +0000

    Sep 4, 2026, 10:40 AM

--- JavaScript files/script12.js
+++ JavaScript files/script12.js
@@ -3,14 +3,14 @@ let button = document.getElementById("btn")
 // https://developer.mozilla.org/en-US/docs/Web/API/Element#mouse_events
 
 
-button.addEventListener("dblclick", () => {
-    document.querySelector(".box").innerHTML = "<b>Yayy you were clicked</b> Enjoy your click!"
-})
+// button.addEventListener("dblclick", () => {
+//     document.querySelector(".box").innerHTML = "<b>Yayy you were clicked</b> Enjoy your click!"
+// })
 
 button.addEventListener("contextmenu", () => {
     alert("Dont hack us by Right click Please")
 })
 
-document.addEventListener("keydown", (e) => {
-    console.log(e, e.key, e.keyCode)
-})
+// document.addEventListener("keydown", (e) => {
+//     console.log(e, e.key, e.keyCode)
+// })

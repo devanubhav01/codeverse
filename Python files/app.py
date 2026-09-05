dbfc3ba1bf8a57d17631a3525d6b0a51c28f5cb8
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-05 10:08:46 +0000

    Sep 5, 2026, 3:38 PM

--- Python files/app.py
+++ Python files/app.py
@@ -5,7 +5,8 @@ app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db=SQLAlchemy(app)
-
+class Todo(db.Model):
+    sno
 
 @app.route('/')
 def hello_world():

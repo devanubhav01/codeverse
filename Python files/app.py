f5d3ecbf47010808262b6bb264d11590847df600
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-05 10:47:38 +0000

    Sep 5, 2026, 4:17 PM

--- Python files/app.py
+++ Python files/app.py
@@ -19,7 +19,7 @@ def __repr__(self)->str:
 
 @app.route('/')
 def hello_world():
-    todo=Todo(title="First Todo",)
+    todo=Todo(title="First Todo",desc="")
     return render_template('index.html')
 #return 'Hello, World!'
 

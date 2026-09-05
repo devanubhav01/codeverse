b48832040865530d90a2649b2a70e00e7e3b247c
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-05 11:18:26 +0000

    Sep 5, 2026, 4:48 PM

--- Python files/app.py
+++ Python files/app.py
@@ -22,7 +22,7 @@ def hello_world():
     if request.method=='POST':
         title=request.form['title']
         desc=request.form['desc']
-    todo=Todo(title="First Todo",desc="Start investing in Stock market")
+    todo=Todo(title=,desc="Start investing in Stock market")
     db.session.add(todo)
     db.session.commit()
     allTodo=Todo.query.all()

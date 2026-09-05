4eff03f370c91d7135215d1933db4c15e93be0ff
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-05 10:10:03 +0000

    Sep 5, 2026, 3:40 PM

--- Python files/app.py
+++ Python files/app.py
@@ -8,6 +8,7 @@ db=SQLAlchemy(app)
 class Todo(db.Model):
     sno=db.Column(db.Integer,primary_key=True)
     title=db.Column(db.String(200),nullable=False)
+    desc=db.Co
 
 @app.route('/')
 def hello_world():

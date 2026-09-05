630dddb813e6466d9b76354f68da7183d1828505
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-05 11:10:40 +0000

    Sep 5, 2026, 4:40 PM

--- Python files/app.py
+++ Python files/app.py
@@ -19,6 +19,8 @@ def __repr__(self)->str:
 
 @app.route('/')
 def hello_world():
+    if request.method=='POST':
+        
     todo=Todo(title="First Todo",desc="Start investing in Stock market")
     db.session.add(todo)
     db.session.commit()

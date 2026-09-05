91e0c5fc94cb7aaa67e78c90d2f58eb7c225db6f
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-05 10:57:10 +0000

    Sep 5, 2026, 4:27 PM

--- Python files/app.py
+++ Python files/app.py
@@ -22,7 +22,7 @@ def hello_world():
     todo=Todo(title="First Todo",desc="Start investing in Stock market")
     db.session.add(todo)
     db.session.commit()
-    
+    allTodo
     return render_template('index.html')
 #return 'Hello, World!'
 

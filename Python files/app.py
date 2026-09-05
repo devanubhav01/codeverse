1d233b7413600d045551b206adde7ba499e5b58c
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-05 10:58:07 +0000

    Sep 5, 2026, 4:28 PM

--- Python files/app.py
+++ Python files/app.py
@@ -24,7 +24,7 @@ def hello_world():
     db.session.commit()
     allTodo=Todo.query.all()
     print(allTodo)
-    return render_template('index.html')
+    return render_template('index.html',allTodo=allTodo)
 #return 'Hello, World!'
 
 @app.route('/show')

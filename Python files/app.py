8677526cb95dcded9fa914aca25fb1eb3418277d
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-05 10:06:03 +0000

    Sep 5, 2026, 3:36 PM

--- Python files/app.py
+++ Python files/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///todo.db"
-app.config['SQLALCHEMY_']="sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_']="sqlite:///todo.db"
 db=SQLAlchemy(app)
 
 

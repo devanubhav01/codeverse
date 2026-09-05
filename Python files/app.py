13a42e41218abd8648218db35f73c27cc3d7e272
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-05 10:11:00 +0000

    Sep 5, 2026, 3:41 PM

--- Python files/app.py
+++ Python files/app.py
@@ -9,7 +9,7 @@ class Todo(db.Model):
     sno=db.Column(db.Integer,primary_key=True)
     title=db.Column(db.String(200),nullable=False)
     desc=db.Column(db.String(500),nullable=False)
-    date_created=db.Column(db)
+    date_created=db.Column(db.DateTime,default=datetime.utcnow)
 
 @app.route('/')
 def hello_world():

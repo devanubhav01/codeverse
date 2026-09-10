7f28a1d680db8d3126a95b40dfe4aa38658b325d
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-10 16:43:30 +0000

    Sep 10, 2026, 10:13 PM

--- C files/P25.c
+++ C files/P25.c
@@ -66,9 +66,9 @@ int main()
     q.arr = (int *)malloc(q.size * sizeof(int));
 
     
-    printf("Dequeuing element %d\n", dequeue(&q));
-    printf("Dequeuing element %d\n", dequeue(&q));
-    printf("Dequeuing element %d\n", dequeue(&q));
+    printf("Enqued element %d\n", dequeue(&q));
+    printf("Enqued element %d\n", dequeue(&q));
+    printf("Enqued element %d\n", dequeue(&q));
 
     printf("Dequeuing element %d\n", dequeue(&q));
     printf("Dequeuing element %d\n", dequeue(&q));

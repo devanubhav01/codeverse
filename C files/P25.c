40c1c8c5069e6aa555f7d4c2aae97fb6c8165253
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-10 16:41:37 +0000

    Sep 10, 2026, 10:11 PM

--- C files/P25.c
+++ C files/P25.c
@@ -37,8 +37,9 @@ int enqueue(struct queue *q, int data)
     {
         q->r = (q->r + 1) % q->size;
         q->arr[q->r] = data;
-        int 
+        int value = q->arr[q->r];
     }
+    
 }
 
 int dequeue(struct queue *q)

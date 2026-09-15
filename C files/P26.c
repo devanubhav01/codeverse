50f2927b2e9ce501882a57f32a18b2603fabffc0
Anubhav Singh <anubhavsingh18599@gmail.com>
2026-09-15 16:46:53 +0000

    Sep 15, 2026, 10:16 PM

--- C files/P26.c
+++ C files/P26.c
@@ -1,32 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node
-{
+struct node {
     int data;
     struct node *link;
 };
+struct node *f = NULL;
+struct node *r = NULL;
 
-void enqueue(struct node *f,struct node *r,int val){
+void print(struct node *head)
+{
+    struct node *ptr = head;
+    while (ptr != NULL)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->link;
+    }
+}
+
+void enqueue(int val){
     struct node *n = (struct node *)malloc(sizeof(struct node));
-     if(n==NULL){
-         printf("Queue is full");
-     }else{
-         n->data = val;
-         n->link = NULL;
-         if(f==NULL){
-             f = r = n;
-         }else{
-             r->link = n;
-             r = n;
-         }
-     }
+    if(n==NULL){
+        printf("Queue is full");
+    }else{
+        n->data = val;
+        n->link = NULL;
+        if(f==NULL){
+            f = r = n;
+        }else{
+            r->link = n;
+            r = n;
+        }
+    }
+}
+
+int dequeue(){
+    int val = -1;
+    struct node *ptr = f;
+    if(f==NULL){
+        printf("Queue is empty");
+    }else{
+        f = f->link;
+        val = ptr->data;
+        free(ptr);
+    }
+    return val;
 }
 
 
+
 int main(){
-    struct node *f = NULL;
-    struct node *r = NULL;
-    enqueue(f, r, 50);
+    
+    enqueue(50);
+    enqueue(55);
+    enqueue(60);
+    printf("%d\n", dequeue());
+    print(f);
+    
+
     return 0;
 }
